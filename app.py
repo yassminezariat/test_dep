09f769f82be08a5d430b2cbdd4a76306e2956d8f
yassminezariat <61391268+yassminezariat@users.noreply.github.com>
2024-06-29 16:43:37 +0100

    a

--- app.py
+++ app.py
@@ -126,4 +126,5 @@ def authregisterbasic():
     return render_template('auth-register-basic.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
